f6c08aca915e8ec77fd32a558be4e1359c1a55a4
AthanasiusOfAlex <lmelahn@legionaries.org>
2023-05-13 08:27:01 +0200

    Create parse.swift

---
+++ parse.swift
@@ -0,0 +1,16 @@
+#!/usr/bin/env swift
+
+import PackageDescription
+
+// let package = Package(
+//     name: "AmazonOrderParser",
+//     dependencies: [
+//         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0"),
+//     ],
+//     targets: [
+//         .executableTarget(
+//             name: "AmazonOrderParser",
+//             dependencies: [ "SwiftSoup" ],
+//             path: "Sources"),
+//     ]
+// )
